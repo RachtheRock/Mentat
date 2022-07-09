386b90090c802a409a322f7f82026de72c45880e
johndpjr <johndpowell02@gmail.com>
2022-07-09 11:02:51 -0500

    configure tests to use lodash

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,12 @@
 import type { Config } from '@jest/types';
+import * as _ from 'lodash';
+
 
 // Sync object
 const config: Config.InitialOptions = {
+  globals: {
+    '_': _
+  },
   preset: 'ts-jest',
   verbose: true,
   moduleFileExtensions: ["ts", "js"],
@@ -11,3 +16,10 @@ const config: Config.InitialOptions = {
   ]
 };
 export default config;
+
+// Or async function
+// export default async (): Promise<Config.InitialOptions> => {
+//   return {
+//     verbose: true,
+//   };
+// };
